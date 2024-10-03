1341231c5f2e5337b7279209d0955f33f2712217
harushimas <naka.tigre3910@gmail.com>
2024-10-03 09:45:31 -0300

    Add files via upload

---
+++ #Aula 1 - python.py
@@ -0,0 +1,18 @@
+#Aula 1 - python
+#Data:03/10 - comentário
+print("programa 1")
+nome_aluno = "fabio"
+idade=16
+vida=0
+print("numero de vidas:",vida)
+vida=vida+2
+print("valor atual de vida:",vida)
+if vida>0:
+      print("você continua no jogo") # executa quando verdade
+      print("bem vindo a proxima fase")
+      energia=100
+else:
+    print("lamento, fim do jogo") # executa quando for falso
+    print("tente novamente")
+    energia=0
+print("meu primeiro programa - python")
